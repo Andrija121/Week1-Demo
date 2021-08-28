041718492fa31dfefd00f63028d2665ba1fe3c52
Boris Sijan <sijanb@outlook.com>
2021-08-28 23:16:10 +0200

    Dodata instanca Car klase zbog testiranja

--- Week1-Demo/Program.cs
+++ Week1-Demo/Program.cs
@@ -6,8 +6,15 @@ namespace Week1_Demo
     {
         static void Main(string[] args)
         {
+            //za ocekivati je da se ispise "167KM/H"
+            //testiraj!!!
+            Car car = new Car("NS567BB", ConsoleColor.Black, 167);
+            Console.WriteLine(car.GetInfo());
+
             Bycicle bycicle = new Bycicle(true, 100);
             Console.WriteLine(bycicle.GetInfo());
+
+
             Console.ReadLine();
             
         }
