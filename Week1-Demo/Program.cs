9674b02f1d454e0acba8f41016a72ea734987523
Andrija Hanga <a.hanga@student.fontys.nl>
2021-08-28 20:28:59 +0200

    Week1-Demo

---
+++ Week1-Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Week1_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bycicle bycicle = new Bycicle(true, 100);
+            Console.WriteLine(bycicle.GetInfo());
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Week1-Demo/Transportation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week1_Demo
+{
+    class Transportation
+    {
+        private int speedKMH;
+        protected int SpeedKMH { get; set; }
+
+        public Transportation(int speedKMH)
+        {
+            SpeedKMH = speedKMH;
+        }
+
+        //public double CalculateHoursTillDestination(int km)
+        //{
+
+        //}
+        public virtual string GetInfo()
+        {
+            return speedKMH + "KMH";
+        }
+
+    }
+    class Car : Transportation
+    {
+        private string licensePlate;
+        private ConsoleColor color;
+
+        public Car(string licensePlate,ConsoleColor color,int speed) : base(speed)
+        {
+            this.licensePlate = licensePlate;
+            this.color = color;
+        }
+    }
+    class Bycicle : Transportation
+    {
+        private bool hasLuggageCarrier;
+
+        public Bycicle(bool hasLuggage,int speed) :  base(speed)
+        {
+            this.hasLuggageCarrier = hasLuggage;
+        }
+        public override string GetInfo()
+        {
+            string info = "Bycicle";
+            info += "- Luggage carrier";
+            if (hasLuggageCarrier)
+                info += " yes";
+            else
+                info += "no";
+
+            info += "-speed:" + base.SpeedKMH;
+            return info;
+            
+        }
+    }
+    class Boat : Transportation
+    {
+        private string company;
+        private int weight;
+
+        public Boat(string company,int weight,int speed) : base(speed)
+        {
+            this.company = company;
+            this.weight = weight;
+        }
+    }
+}
